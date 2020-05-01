9ef1ca17e141fd4921a0463458aaa79d92915533
SoumyaSriShekar <63279265+SoumyaSriShekar@users.noreply.github.com>
2020-05-01 17:01:15 +1000

    Update PropertyManager.java

--- PropertyManager.java
+++ PropertyManager.java
@@ -1,26 +1,109 @@
+import java.util.ArrayList;
 
 public class PropertyManager extends Employee
 {
-	private static int pmCounter = 0;
-	
-	public PropertyManager(String name, String type, double salary, double hourlyRate)
+	private String property;
+	private ArrayList<Inspection> inspection;
+	private boolean createInspection;
+	public PropertyManager(String ID,String password , String name, String type, double salary, double hourlyRate)
 	{
-		super(name, type, salary, hourlyRate);
+		super(ID, password, name, type, salary, hourlyRate);
+		property = null;
+		createInspection = false;
+		inspection = new ArrayList<Inspection>();
 	}
 	
-	public String setID()
+	
+	public void advertiseProperty(Rental prop)
 	{
-		return " ";
+		if(this.getID().equalsIgnoreCase(prop.getPropertyManager()))
+		{
+			System.out.println("The property details are: \n"+prop);
+		}
+		else
+		{
+			System.out.println("You cannot advertise the property as you are not assigned to it");
+		}
+	}
+	public void createInspection(Rental rental)
+	{
+		if(this.getID().equalsIgnoreCase(rental.getPropertyManager()))
+		{
+			System.out.println(this.getID());
+			System.out.println(rental.getPropertyManager());
+			
+			inspection.add(new Inspection (rental, "22/08/1994", this.getID()));
+			System.out.println("Inspection created successfully");
+			createInspection = true;
+		}
+		else
+		{
+			System.out.println("Inspection cannot be created for property "+rental.getPropertyID());
+		}
 	}
 	
-	public void advertiseProperty()
+	public void conductInspection(String inspectionID)
 	{
-		
+		int index = searchInspection(inspectionID);
+		if(index >=0 && createInspection && inspection.get(index).getStatus().equalsIgnoreCase("Created"))
+		{
+			System.out.println("Inspection conducted successfully");
+		}
+		else
+		{
+			System.out.println("Inspection not conducted successfully");
+		}
 	}
-	public void conductInspection()
+	
+	public void cancelInspection(String inspectionID)
 	{
-		
+		int index = searchInspection(inspectionID);
+		if(index >= 0 && createInspection)
+		{
+			inspection.get(index).setStatus("Cancel");
+			System.out.println("Inspection is cancelled");
+		}
+		else
+		{
+			System.out.println(index);
+			System.out.println(createInspection);
+			System.out.println("Inspection is not cancelled");
+		}
 	}
+	public int searchInspection(String inspectionID)
+	{
+		int index = -1;
+		for(int i = 0; i < inspection.size(); i++)
+		{
+			if(inspectionID.equalsIgnoreCase(inspection.get(i).getInspectionID()))
+			{
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+	
+	
+	
+	
+	public ArrayList<Inspection> getInspection() 
+	{
+		return inspection;
+	}
+
+	public String getProperty() 
+	{
+		return property;
+	}
+
+
+	public void setProperty(String property) 
+	{
+		this.property = property;
+	}
+
+
 	public boolean reviewApplication()
 	{
 		return true;
@@ -29,9 +112,9 @@ public class PropertyManager extends Employee
 	{
 		return true;
 	}
-	public void organiseWork()
+	public boolean organiseWork()
 	{
-		
+		return true;
 	}
 	public void payBill()
 	{
