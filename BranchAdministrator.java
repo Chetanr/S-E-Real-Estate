516e6ffa840975034ed86a7b71e9f86c59a78b45
SoumyaSriShekar <63279265+SoumyaSriShekar@users.noreply.github.com>
2020-05-01 16:59:55 +1000

    Update BranchAdministrator.java

--- BranchAdministrator.java
+++ BranchAdministrator.java
@@ -1,25 +1,19 @@
 
 public class BranchAdministrator extends Employee
 {
-	private static int baCounter = 0;
-	
-	public BranchAdministrator(String name, String type, double salary, double hourlyRate)
+	public BranchAdministrator(String ID,String password , String name, String type, double salary, double hourlyRate)
 	{
-		super(name, type, salary, hourlyRate);
+		super(ID, password, name , type, salary, hourlyRate);
 	}
-	
-	public String setID()
-	{
-		return " ";
-	}
-	
+
 	public boolean receiveDoc()
 	{
 		return true;
 	}
-	public boolean scanDoc()
+	public void scanDoc(Customer customer, String document)
 	{
-		return true;
+		customer.setDocuments(document);
+		
 	}
 	public void collectRent()
 	{
