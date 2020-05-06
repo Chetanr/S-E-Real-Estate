70390f5a3a621feb24e3e49ac88aaeb6739b9bc0
miadnasim <53205877+miadnasim@users.noreply.github.com>
2020-05-06 13:21:45 +1000

    Add files via upload

---
+++ src/Inspection.java
@@ -0,0 +1,14 @@
+
+public class Inspection {
+	String date;
+	String time;
+	String property_id;
+	public Inspection(String property_id,String time, String date ) {
+		this.date = date;
+		this.property_id = property_id;
+		this.time = time;
+	}
+	String scheduleinspection() {return "";}
+	
+	
+}
