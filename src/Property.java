3651b5b2595049e02c16bf6307f792efcc004ce7
Kunal <54241991+kunalpahuja@users.noreply.github.com>
2020-05-11 18:52:46 +1000

    Update Property.java

--- src/Property.java
+++ src/Property.java
@@ -1,31 +1,116 @@
 abstract class Property 
 {
-	private static int property_id;
-	private static String address;
-	private static String suburb;
-	private int num_bed;
-	private int num_bath;
-	private int num_car_space;
-	private String house_type;
-	private String tenant_name;
+	private String propertyID;
+	private String address;
+	private String suburb;
+	private int numBed;
+	private int numBath;
+	private int numCarSpace;
+	private String houseType;
+	private String tenantName;
+	private String propertyOwnerID;
+	//private Customer cus;
 	
-	
+
 	//abstract class to negotiate the offer between buyer and seller
 	public abstract void negotiateOffer();
-	
-	
-	//transfer the property contract to tenant
-	public abstract void finaliseOffer();
 
+	@Override
+	public String toString() {
+		return "Property [propertyID=" + propertyID + ", address=" + address + ", numBed=" + numBed + ", numBath="
+				+ numBath + ", numCarSpace=" + numCarSpace + ", houseType=" + houseType + ", tenantName="
+				+ tenantName + ", propertyOwnerID=" + propertyOwnerID + "]";
+	}
 
-	public String getTenant_name() {
-		return tenant_name;
+	public Property(String propertyID, String address, int numBed, int numBath, int numCarSpace, String houseType,
+			String tenantName, String propertyOwnerID) {
+		super();
+		this.propertyID = propertyID;
+		this.address = address;
+		this.numBed = numBed;
+		this.numBath = numBath;
+		this.numCarSpace = numCarSpace;
+		this.houseType = houseType;
+		this.tenantName = tenantName;
+		this.propertyOwnerID = propertyOwnerID;
 	}
 
 
-	public void setTenant_name(String tenant_name) {
-		this.tenant_name = tenant_name;
+
+	public String getpropertyID() {
+		return propertyID;
 	}
+
+	public void setpropertyID(String propertyID) {
+		this.propertyID = propertyID;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getSuburb() {
+		return suburb;
+	}
+
+	public void setSuburb(String suburb) {
+		this.suburb = suburb;
+	}
+
+	public int getnumBed() {
+		return numBed;
+	}
+
+	public void setnumBed(int numBed) {
+		this.numBed = numBed;
+	}
+
+	public int getnumBath() {
+		return numBath;
+	}
+
+	public void setnumBath(int numBath) {
+		this.numBath = numBath;
+	}
+
+	public int getnumCarSpace() {
+		return numCarSpace;
+	}
+
+	public void setnumCarSpace(int numCarSpace) {
+		this.numCarSpace = numCarSpace;
+	}
+
+	public String gethouseType() {
+		return houseType;
+	}
+
+	public void sethouseType(String houseType) {
+		this.houseType = houseType;
+	}
+
+	public String gettenantName() {
+		return tenantName;
+	}
+
+	public void settenantName(String tenantName) {
+		this.tenantName = tenantName;
+	}
+
+	public String getPropertyOwnerID() {
+		return propertyOwnerID;
+	}
+
+	public void setPropertyOwnerID(String propertyOwnerID) {
+		this.propertyOwnerID = propertyOwnerID;
+	}
+
+
+	
 	
 	
 }
