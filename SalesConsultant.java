0d4ea0e0718015b222dd9cfab874888e3ce70465
SoumyaSriShekar <63279265+SoumyaSriShekar@users.noreply.github.com>
2020-05-22 17:23:07 +1000

    Update SalesConsultant.java

--- SalesConsultant.java
+++ SalesConsultant.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.*;
 public class SalesConsultant extends Employee
 {
 	private static final double RATE = 0.4;
@@ -16,62 +16,74 @@ public class SalesConsultant extends Employee
 	}
 
 	
-	public void advertiseProperty(Sale sale)
+	public void createInspection(Sale sale, String date) throws PropertyNotAssignedToEmployee
 	{
-		if(this.getID().equalsIgnoreCase(sale.getSalesConsultant().getID()))
+		if(sale.getSalesConsultant() == null)
 		{
-			System.out.println("The property details are: \n"+sale);
+			throw new PropertyNotAssignedToEmployee("Inspection cannot be created as property not assigned to an employee");
 		}
-		else
+		else if(this.getID().equalsIgnoreCase(sale.getSalesConsultant().getID()) && sale.getStatus().equalsIgnoreCase("Open"))
 		{
-			System.out.println("You cannot advertise the property as you are not assigned to it");
-		}
-	}
-	public void liaseVendor()
-	{
-		
-	}
-	public void createInspection(Sale sale)
-	{
-		String salesConsultantID = sale.getSalesConsultant().getID();
-		if(this.getID().equalsIgnoreCase(salesConsultantID))
-		{
-			
-			inspection.add(new Inspection (sale, "22/08/1994", this.getID()));
+			inspection.add(new Inspection (sale, date, this.getID()));
 			System.out.println("Inspection created successfully");
 			createInspection = true;
 		}
+		else if(!(this.getID().equalsIgnoreCase(sale.getSalesConsultant().getID())))
+		{
+			System.out.println("Only employee assigned to property can create inspection");
+		}
 		else
 		{
-			System.out.println("Inspection cannot be created for property "+sale.getPropertyID());
+			System.out.println("Inspection cannot be created as property is under contract");
 		}
 	}
 	
 	public void conductInspection(String inspectionID)
 	{
 		int index = searchInspection(inspectionID);
-		if(index >=0 && createInspection && inspection.get(index).getStatus().equalsIgnoreCase("Created"))
+		if(createInspection && index >=0 && inspection.get(index).getStatus().equalsIgnoreCase("Created"))
 		{
+		    inspection.get(index).setStatus("Conducted");
 			System.out.println("Inspection conducted successfully");
 		}
+		else if(index < 0)
+		{
+			System.out.println("Inspection id cannot be found");
+		}
 		else
 		{
-			System.out.println("Inspection not conducted successfully");
+			System.out.println("Inspection cannot be conducted as it cancelled/already conducted");
 		}
+		
+		
 	}
 	
-	public void cancelInspection(String inspectionID)
+	public void cancelInspection(Sale sale) throws PropertyNotAssignedToEmployee
 	{
-		int index = searchInspection(inspectionID);
-		if(index >= 0 && createInspection)
+		if(sale.getSalesConsultant() == null)
 		{
-			inspection.get(index).setStatus("Cancel");
-			System.out.println("Inspection is cancelled");
+			throw new PropertyNotAssignedToEmployee("Inspection cannot be cancelled as property not assigned to an employee");
 		}
-		else
+		else if(!(this.getID().equalsIgnoreCase(sale.getSalesConsultant().getID())))
 		{
-			System.out.println("Inspection cannot be cancelled");
+			System.out.println("Only employee assigned to property can create inspection");
 		}
+		else if(sale.getStatus().equalsIgnoreCase("Open"))
+		{
+			System.out.println("The property should be 'under contract' for inspections to be cancelled");
+		}
+		else if(this.getID().equalsIgnoreCase(sale.getSalesConsultant().getID()) && sale.getStatus().equalsIgnoreCase("under contract"))
+		{
+			for(int i = 0; i < inspection.size(); i++)
+			{
+				if(sale.getPropertyID().equalsIgnoreCase(inspection.get(i).getPropID()))
+				{
+					System.out.println("Inspection cancelled successfully");
+					inspection.get(i).setStatus("Cancelled");
+				}
+			}
+		}
+		
 	}
 	
 	public int searchInspection(String inspectionID)
@@ -88,15 +100,16 @@ public class SalesConsultant extends Employee
 		return index;
 	}
 	
-	public ArrayList<Inspection> getInspection() 
-	{
-		return inspection;
-	}
-
-	public void negotiate()
+	public void printInspection() 
 	{
 		
+			for (int i = 0; i < inspection.size(); i++) 
+			{
+				System.out.println(inspection.get(i).toString());
+			}
+		
 	}
+
 	public double getRate()
 	{
 		return RATE;
@@ -106,6 +119,10 @@ public class SalesConsultant extends Employee
 	{
 		return this.properties;
 	}
+	public ArrayList<Inspection> getInspection() 
+	{
+		return this.inspection;
+	}
 
 
 	public void setProperty(Sale property) 
@@ -114,23 +131,47 @@ public class SalesConsultant extends Employee
 		
 	}
 	
-	public void calculateCommission()
+	public double totalCommission()
 	{
+		commission = 0;
 		for(int i = 0; i < properties.size(); i++)
 		{
 			commission += properties.get(i).getSalesConsultantCommission();
 		}
+		return commission;
+	}
+	public double getCommission()
+	{
+		return this.commission;
 	}
 	@Override
 	public void calculateMonthlySalary()
 	{
+		double saleConsultantCommission = totalCommission();
 		if(getType().equalsIgnoreCase("PT"))
 		{
-			setSalaryForMonth((getHourlyRate() * getHours()) + commission);
+			setSalaryForMonth((getHourlyRate() * getHours()) + saleConsultantCommission);
 		}
 		else
 		{
-			setSalaryForMonth((getSalary() * getHours()) + commission);
+			setSalaryForMonth((getSalary() * getHours()) + saleConsultantCommission);
+		}
+	}
+	
+	@Override
+	public void printDetails()
+	{
+		super.printDetails();
+		if(getProperty().isEmpty())
+			System.out.println("No Property assigned to employee");
+		else
+		{
+			System.out.print("Property Assigned:");
+			for(int i = 0; i < getProperty().size(); i++)
+			{
+				System.out.print(getProperty().get(i).getPropertyID()+" ");
+			}
+			System.out.println();
 		}
 	}
 	
