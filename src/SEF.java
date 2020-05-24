ceedf6e80728e06a0e4c8fda060ce199b97b977e
miadnasim <53205877+miadnasim@users.noreply.github.com>
2020-05-24 16:31:41 +1000

    Add files via upload

--- src/SEF.java
+++ src/SEF.java
@@ -1,5 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,9 +14,94 @@ public class SEF {
 	static ArrayList<Inspection> inspectionList = new ArrayList<Inspection>();
 	
 	public static void main(String[] args) {
+		initializeData();
 		DisplayMainMunu();
 	}
 	
+	private static void initializeData() {
+		try {
+			File custFile = new File("Customer.txt");
+		    Scanner read = new Scanner(custFile);
+		    read.useDelimiter("\\n|,");
+		    while(read.hasNext()){
+		       String custType  = read.next().replaceAll("\\r|\\n", "");
+		       String custId    = read.next().replaceAll("\\r|\\n", "");
+		       String custName  = read.next().replaceAll("\\r|\\n", "");
+		       String surName   = read.next().replaceAll("\\r|\\n", "");
+		       String custEmail = read.next().replaceAll("\\r|\\n", "");
+		       String custPass  = read.next().replaceAll("\\r|\\n", "");
+		       
+		       String custIncome = read.next().replaceAll("\\r|\\n", "");
+		       String custOccu = read.next().replaceAll("\\r|\\n", "");
+		       String custEmployer = read.next().replaceAll("\\r|\\n", "");
+		       
+				if(custType.equals("1")) {
+					Customer customerObj = new Vendor(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
+					customerList.add(customerObj);
+				}
+				else if(custType.equals("2")) {
+					Customer customerObj = new Landlord(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
+					customerList.add(customerObj);
+				}
+				else if(custType.equals("3")) {
+					Customer customerObj = new Rentee(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
+					customerList.add(customerObj);
+				}
+				else if(custType.equals("4")) {
+					Customer customerObj = new Buyer(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
+					customerList.add(customerObj);
+				}
+		    }
+		    
+		    //property
+			File propFile = new File("Property.txt");
+		    Scanner propRead = new Scanner(propFile);
+		    propRead.useDelimiter("\\n|,");
+		    while(propRead.hasNext()){
+		        String propertyId      = propRead.next().replaceAll("\\r|\\n", "");
+		        String propAddress     = propRead.next().replaceAll("\\r|\\n", "");
+		        int propBed            = Integer.parseInt(propRead.next().replaceAll("\\r|\\n", ""));
+		        int propBath           = Integer.parseInt(propRead.next().replaceAll("\\r|\\n", ""));
+		        int propSpace          = Integer.parseInt(propRead.next().replaceAll("\\r|\\n", ""));
+		        String houseType       = propRead.next().replaceAll("\\r|\\n", "");
+		        String propTenantName  = propRead.next().replaceAll("\\r|\\n", "");
+		        String propOwner       = propRead.next().replaceAll("\\r|\\n", "");
+		       
+				Property propertyObj = new Property(propertyId, propAddress, propBed, propBath, propSpace, houseType, propTenantName, propOwner );
+				propertyList.add(propertyObj);
+		    }
+
+		    //inspection
+			File insFile = new File("Inspection.txt");
+		    Scanner insRead = new Scanner(insFile);
+		    insRead.useDelimiter("\\n|,");
+		    while(insRead.hasNext()){
+		        String inspectionId     = insRead.next().replaceAll("\\r|\\n", "");
+		        String insDescription   = insRead.next().replaceAll("\\r|\\n", "");
+		        String insDate          = insRead.next().replaceAll("\\r|\\n", "");
+		        String insTime          = insRead.next().replaceAll("\\r|\\n", "");
+		        String insAgentId       = insRead.next().replaceAll("\\r|\\n", "");
+		        String insPropId        = insRead.next().replaceAll("\\r|\\n", "");
+	       
+		        Property propertyObj = new Property();
+				for(int i = 0; i < propertyList.size(); i++)
+				{
+					if(propertyList.get(i).getProperty_id().equals(insPropId) ) {
+						propertyObj = propertyList.get(i); 
+					}
+				}
+		        
+				Inspection inspectionObj = new Inspection(inspectionId, insDescription, insDate, insTime, insAgentId, propertyObj );
+				inspectionList.add(inspectionObj);
+				System.out.println("AFTER READ FILE: " + inspectionList.size());
+		    }
+
+		}catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+	}
+
 	public static void DisplayMainMunu() {
 		Scanner in = new Scanner(System.in);
 		System.out.println("** Real State System **");
@@ -42,41 +131,51 @@ public class SEF {
 		
 	}
 	
-	public static void login() throws FileNotFoundException {
+	public static void login() throws IOException {
 
-	    
+		//String abc = getCustomerId("1");
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter username: ");
 		String userName = in.nextLine();
+		userName= userName.replaceAll("\\r|\\n", "");
 		
 		System.out.println("Enter password: ");
 		String password = in.nextLine();
+		password= password.replaceAll("\\r|\\n", "");
 		
 		File loginf = new File("login.txt");
 		
 	    Scanner read = new Scanner(loginf);
 	    read.useDelimiter("\\n|,");
-	    boolean login = false;
+	    boolean loginFlag = false;
 	    while(read.hasNext()){
-	       String user = read.next();
-	       String pass = read.next();
-	       //System.out.println(user + " " + pass);
-	       
-	       if(user == userName && pass == password) {
-	    	   displayMenu(userName);
+	       String user = read.next().replaceAll("\\r|\\n", "");
+	       String pass = read.next().replaceAll("\\r|\\n", "");
+	       if(userName.equals(user)  && password.equals(pass)) {
+	    	   loginFlag = true;
+	    	   break;
 	       }
 	    }
 	    
-		//displayMenu(userName);
-		in.close();
+	    if(loginFlag == false) {
+	    	System.out.println("Wrong username and password.");
+	    	DisplayMainMunu();
+	    }
+	    
+	    if(loginFlag == true) {
+	    	displayMenu(userName);
+	    }
+		
+	    in.close();
 	}
 	
 	public static void logout() {
 		System.exit(0);
 	}
 	
-	public static void displayMenu(String userName) {
+	public static void displayMenu(String userName) throws FileNotFoundException, IOException {
 		Scanner in = new Scanner(System.in);
+		
 		System.out.println("1. Add Customer");
 		System.out.println("2. Add Property");
 		System.out.println("3. Add Inspection");
@@ -84,26 +183,34 @@ public class SEF {
 		System.out.println("5. Display Property");
 		System.out.println("6. Display Inspection");
 		
+		System.out.println("Enter your choice: ");
+		
 		String userChioce = in.nextLine();
 		
-		if(userChioce == "1") {
+		if(userChioce.equals("1") ) {
 			addCustomer(userName);
-		} else if(userChioce == "2") {
+		} else if(userChioce.equals("2")) {
 			addProperty(userName);
-		} else if(userChioce == "3") {
+		} else if(userChioce.equals("3")) {
 			addInspection(userName);
-		} else if(userChioce == "4") {
+		} else if(userChioce.equals("4")) {
 			displayCustomer(userName);
-		} else if(userChioce == "5") {
+		} else if(userChioce.equals("5")) {
 			displayProperty(userName);
-		} else if(userChioce == "6") {
+		} else if(userChioce.equals("6")) {
 			displayInspection(userName);
 		}
 		
 		in.close();
 	}
 	
-	public static void addCustomer(String userName) {
+	/* 
+	 * Name : addCustomer
+	 * Purpose: Customer info to save
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	public static void addCustomer(String userName) throws FileNotFoundException , IOException {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Enter Name: ");
@@ -132,32 +239,69 @@ public class SEF {
 
 		String custId = getCustomerId(custType);
 		
-		if(custType == "1") {
+		String customerStr = "";
+		
+		if(custType.equals("1")) {
 			Customer customerObj = new Vendor(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
 			customerList.add(customerObj);
 			System.out.println("Success! Vendor has been created with id "+custId);
+			customerStr = customerObj.toString();
+			
+			customerStr = custType +","+ customerStr;
 		}
-		else if(custType == "2") {
+		else if(custType.equals("2")) {
 			Customer customerObj = new Landlord(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
 			customerList.add(customerObj);
 			System.out.println("Success! Landlord has been created with id "+custId);
+			
+			customerStr = customerObj.toString();
+			
+			customerStr = custType +","+ customerStr;
 		}
-		else if(custType == "3") {
+		else if(custType.equals("3")) {
 			Customer customerObj = new Rentee(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
 			customerList.add(customerObj);
 			System.out.println("Success! Rentee has been created with id "+custId);
+			
+			customerStr = customerObj.toString();
+			
+			customerStr = custType +","+ customerStr;
 		}
-		else if(custType == "4") {
+		else if(custType.equals("4")) {
 			Customer customerObj = new Buyer(custId, custName, surName, custEmail, custPass, custIncome, custOccu, custEmployer );
 			customerList.add(customerObj);
 			System.out.println("Success! Buyer has been created with id "+custId);
+			
+			customerStr = customerObj.toString();
+			
+			customerStr = custType +","+ customerStr;
 		}
 		
-		in.close();
+		try
+		{
+		    String filename= "Customer.txt";
+		    FileWriter fw = new FileWriter(filename,true);
+		    fw.write(customerStr + "\n");
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+		
 		displayMenu(userName);
+		in.close();
+		
 	}
 	
-	public static void addProperty(String userName) {
+	
+	/* 
+	 * Name : addProperty
+	 * Purpose: get property info to save
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	public static void addProperty(String userName) throws FileNotFoundException, IOException {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Enter address: ");
@@ -185,15 +329,31 @@ public class SEF {
 		
 		Property propertyObj = new Property(propertyId, propAddress, propBed, propBath, propSpace, houseType, propTenantName, propOwner );
 		propertyList.add(propertyObj);
-		System.out.println("Success! Vendor has been created with id "+propertyId);
+		System.out.println("Success! Property has been created with id "+propertyId);
 
-		
-		in.close();
+		try
+		{
+		    String filename= "Property.txt";
+		    FileWriter fw = new FileWriter(filename,true);
+		    fw.write(propertyObj.toString() + "\n");
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
 		
 		displayMenu(userName);
+		in.close();
 	}
 	
-	public static void addInspection(String userName) {
+	/* 
+	 * Name : addInspection
+	 * Purpose: save inspection
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	public static void addInspection(String userName) throws FileNotFoundException, IOException {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Enter Property Id: ");
@@ -223,36 +383,94 @@ public class SEF {
 		inspectionList.add(inspectionObj);
 		System.out.println("Success! inspection has been created with id "+inspectionId);		
 		
-		in.close();
+		try
+		{
+		    String filename= "Inspection.txt";
+		    FileWriter fw = new FileWriter(filename,true);
+		    fw.write(inspectionObj.toString() + "\n");
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
 		
 		displayMenu(userName);
+		in.close();
 	}
 	
-	private static void displayInspection(String userName) {
+	/* 
+	 * Name : displayInspection
+	 * Purpose: Display Inspection List
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	private static void displayInspection(String userName) throws FileNotFoundException, IOException {
+		System.out.println("FROM DISPLAY INSPECTION............");
 		for(int i = 0; i < inspectionList.size(); i++)
 		{
-			//Display the inspection
+			System.out.println("ID: "+inspectionList.get(i).getInspectionId() +" "+ 
+				"Description: "+inspectionList.get(i).getDescription() +" "+
+				"Date: "+inspectionList.get(i).getDate() +" "+
+				"Time: "+ inspectionList.get(i).getTime()
+			);
 		
 		}		
 		
 		displayMenu(userName);
 	}
 
-	private static void displayProperty(String userName) {
+	/* 
+	 * Name : displayProperty
+	 * Purpose: Display Property List
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	private static void displayProperty(String userName) throws FileNotFoundException, IOException {
 		for(int i = 0; i < propertyList.size(); i++)
 		{
-		    //Display the property	
+			System.out.println("Property ID: "+propertyList.get(i).getProperty_id() +" "+ 
+				"Address: "+propertyList.get(i).getAddress() +" "+
+				"Bed: "+propertyList.get(i).getNum_bed() +" "+
+				"Bath: "+propertyList.get(i).getNum_bath() +" "+
+				"Space: "+propertyList.get(i).getNum_car_space() +" "+
+				"Type: "+propertyList.get(i).getHouse_type() +" "+
+				"Tanent Name: "+propertyList.get(i).getTenant_name() +" "+
+				"Owner ID: "+propertyList.get(i).getPropertyOwnerID()
+			);
 		
 		}
 		
 		displayMenu(userName);
 	}
-
-	private static void displayCustomer(String userName) {
 	
+	/* 
+	 * Name : displayCustomer
+	 * Purpose: Display Customer List
+	 * Param: logged In user Name
+	 * return: none
+	 */
+	private static void displayCustomer(String userName) throws FileNotFoundException, IOException {
+	
+		String custType = "";
 		for(int i = 0; i < customerList.size(); i++)
 		{
-			//Display the Customer
+			if(customerList.get(i).toString().matches(".*Vendor.*")) {
+				custType = "Vendor";
+			} else if(customerList.get(i).toString().matches(".*Landlord.*")) {
+				custType = "Landlord";
+			} else if(customerList.get(i).toString().matches(".*Rentee.*")) {
+				custType = "Rentee";
+			} else if(customerList.get(i).toString().matches(".*Buyer.*")) {
+				custType = "Buyer";
+			}
+			
+			System.out.println("ID:"+customerList.get(i).getCustomer_id() +" "+ 
+				"FirstName:"+customerList.get(i).getFirstname() +" "+ 
+				"SurName:"+customerList.get(i).getSurename() +" "+
+				"Email:"+customerList.get(i).getEmail() +" "+
+				"Type:"+custType
+			);
 		
 		}
 		
@@ -263,7 +481,7 @@ public class SEF {
 		int count = 1;
 		String classType  = "";
 		String customerId = ""; 
-		if(type == "1") {
+		if(type.equals("1")) {
 			classType = "Vendor";
 			for(int i = 0; i < customerList.size(); i++)
 			{
@@ -272,7 +490,7 @@ public class SEF {
 				}
 			}
 			customerId = "VEN"+String.format("%03d", count);
-		} else if(type == "2") {
+		} else if(type.equals("2")) {
 			classType = "Landlord";
 			for(int i = 0; i < customerList.size(); i++)
 			{
@@ -281,7 +499,7 @@ public class SEF {
 				}
 			}
 			customerId = "LAN"+String.format("%03d", count);
-		} else if(type == "3") {
+		} else if(type.equals("3")) {
 			classType = "Rentee";
 			for(int i = 0; i < customerList.size(); i++)
 			{
@@ -290,7 +508,7 @@ public class SEF {
 				}
 			}
 			customerId = "REN"+String.format("%03d", count);
-		} else if(type == "4") {
+		} else if(type.equals("4")) {
 			classType = "Buyer";
 			for(int i = 0; i < customerList.size(); i++)
 			{
@@ -300,7 +518,7 @@ public class SEF {
 			}
 			customerId = "BUY"+String.format("%03d", count);
 		}
-						
+//        System.out.println(customerId);
 		return customerId;
 	}
 	
