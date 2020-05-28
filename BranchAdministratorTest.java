985795476b6d19773ea11a7201d415f3c231e76c
SoumyaSriShekar <63279265+SoumyaSriShekar@users.noreply.github.com>
2020-05-28 20:35:58 +1000

    Update BranchAdministratorTest.java

--- BranchAdministratorTest.java
+++ BranchAdministratorTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,9 +12,10 @@ import org.junit.Test;
 
 public class BranchAdministratorTest 
 {
-	BranchAdministrator ba1;
-	BranchAdministrator ba2;
-	BranchManager bm;
+	BranchAdministrator branchAdministrator;
+	Employee propertyManager;
+	BranchManager branchManager;
+	ArrayList<Employee> employees;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception 
 	{
@@ -29,69 +32,49 @@ public class BranchAdministratorTest
 	public void setUp() throws Exception 
 	{
 	
-		ba1 = new BranchAdministrator("ba001", "welcome123","James Isaac", "PT", 0.0, 25.0);  //Part Time employee
-		ba2 = new BranchAdministrator("ba002", "welcome456","John Lever", "FT", 2000.0, 0.0); //Full time employee
-		bm = new BranchManager("bm001","welcome789","Branch Manager", "FT", 5000.0, 0.0);
+		branchAdministrator = new BranchAdministrator("ba001", "welcome123","James Isaac", "PT", 0.0, 25.0);
+		propertyManager = new PropertyManager("PM001", "welcome12***3","Jason White", "FT", 5000, 0);
+		branchManager = new BranchManager("BM001", "welcome123","Branch Manager", "FT", 5000, 0);
+		employees = new ArrayList<>();
+		employees.add(propertyManager);
+		employees.add(branchAdministrator);
 	}
+	
 	@After
 	public void tearDown() throws Exception 
 	{
 		System.out.println("After each method");
 	}
 	
-	//Calculate salary for Part time Employees
-	@Test
-	public void testCalculateSalary()  throws Exception
+	//Negative JUnit test case where an exception occurs when payroll is run at date that is not end of month
+	@Test(expected = PayrollException.class)
+	public void testPayroll1() throws Exception
 	{
-		ba1.enterHours(bm);      //Entering hours as 10
-		ba1.viewSalary();
-		assertEquals(ba1.getSalary(), 250);
-		System.out.println("Test successful");
+		branchAdministrator.currentDate();
+		branchAdministrator.payroll(employees);
 	}
-
-	//Calculate salary for Full time Employees
+	
+	//Positive Junit test showing salary calculated for month for Full time employee
+	//after running payroll
 	@Test
-	public void testCalculateSalary1()  throws Exception
+	public void testPayroll2() throws Exception
 	{
-		ba2.enterHours(bm);      //Entering hours as 10
-		ba2.viewSalary();
-		assertEquals(ba2.getSalary(), 2000);
-		System.out.println("Test successful");
+		branchAdministrator.advanceDate(3);
+		branchAdministrator.payroll(employees);
+		assertEquals(employees.get(0).getSalaryForMonth(), 5000);
+	}
+	
+	//Positive Junit test showing salary calculated for month for part time employee after
+	//running payroll
+	@Test
+	public void testPayroll3() throws Exception
+	{
+		employees.get(1).setHours(10);
+		branchAdministrator.advanceDate(3);
+		branchAdministrator.payroll(employees);
+		assertEquals(employees.get(1).getSalaryForMonth(), 250);
 	}
 	
 
-	
-	@Test
-	public void testCollectRent() throws Exception
-	{
-		fail("collectRent() Method not yet implemented");
-		
-	}
 
-	@Test
-	public void testCreditAmt() throws Exception
-	{
-		fail("creditAmount() Method not yet implemented");
-		
-	}
-	
-	@Test
-	public void testPayroll() throws Exception
-	{
-		fail("payroll() Method not yet implemented");
-		
-	}
-
-	
-	@Test
-	public void testScanDoc() throws Exception
-	{
-		fail("scanDoc() Method not yet implemented");
-	}
-	
-	@Test
-	public void testReceiveDoc() throws Exception
-	{
-		fail("receiveDoc() not yet implemented");
-	}
 }
