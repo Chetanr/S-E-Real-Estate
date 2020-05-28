320d4d2512e7dee04eb7bc32a3ea7cf33fe97a14
SoumyaSriShekar <63279265+SoumyaSriShekar@users.noreply.github.com>
2020-05-28 20:39:59 +1000

    Update PropertyManagerTest.java

--- PropertyManagerTest.java
+++ PropertyManagerTest.java
@@ -10,7 +10,8 @@ import org.junit.Test;
 
 public class PropertyManagerTest 
 {
-	PropertyManager pm;
+	PropertyManager propertyManager;
+	Rental rental;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception 
@@ -28,7 +29,9 @@ public class PropertyManagerTest
 	public void setUp() throws Exception 
 	{
 	
-		pm = new PropertyManager("pm001", "welcome123","Joseph Isaac", "FT", 90000.0, 0.0);
+		System.out.println("Before each method");
+		propertyManager = new PropertyManager("pm001", "welcome123","James Isaac", "PT", 0, 25);
+		rental = new Rental("Henry Street", "Kensington", 2,2,2,"house");
 	}
 	@After
 	public void tearDown() throws Exception 
@@ -36,52 +39,49 @@ public class PropertyManagerTest
 		System.out.println("After each method");
 	}
 
+	//Positive JUnit test case where employee assigned to property creates inspection
 	@Test
-	public void testAdvertiseProperty() throws Exception
+	public void testCreateInspection1() throws Exception
 	{
-		fail("advertiseProperty() Method not yet implemented");
+		rental.setPropertyManager(propertyManager);
+		propertyManager.createInspection(rental, "05/06/2020");
+		assertEquals(propertyManager.getInspection().get(0).getInspectionID(), "INS002");
+	}
 		
-	}
-
-	@Test
-	public void testConductInsp() throws Exception
+	//Negative JUnit test case when trying to create inspection on property that is not assigned to an employee
+	@Test(expected = PropertyNotAssignedToEmployee.class)
+	public void testCreateInspection2() throws Exception
 	{
-		fail("conductInspection() Method not yet implemented");
+		propertyManager.createInspection(rental, "05/06/2020");
+	}
+	
+	//Positive Junit test case where employee assigned to property conducts inspection
+	@Test
+	public void testConductInspection1() throws Exception
+	{
+		rental.setPropertyManager(propertyManager);
+		propertyManager.createInspection(rental, "05/06/2020");
+		propertyManager.conductInspection("INS001");
+		assertEquals(propertyManager.getInspection().get(0).getStatus(), "Conducted");
+	}
+	
+	//Negative JUnit test case where inspection is tried to be cancelled on property that is not assigned to an employee
+	@Test(expected = PropertyNotAssignedToEmployee.class)
+	public void testCancelInspection1() throws Exception
+	{
+		propertyManager.cancelInspection(rental);
+	}
 		
-	}
-	
+	//Positive Junit test case where employee assigned to property cancels inspection
+	//after property is let
 	@Test
-	public void testOrganiseWork() throws Exception
+	public void testCancelInspection2() throws Exception
 	{
-		fail("organisework() Method not yet implemented");
-		
+		rental.setPropertyManager(propertyManager);
+		propertyManager.createInspection(rental, "05/07/2020");
+		rental.setStatus("let");
+		propertyManager.cancelInspection(rental);
+		assertEquals(propertyManager.getInspection().get(0).getStatus(), "Cancelled");
 	}
-	
-	@Test
-	public void testPayBill() throws Exception
-	{
-		fail("payBill() Method not yet implemented");
-		
-	}
-	
-	@Test
-	public void testDeductFee() throws Exception
-	{
-		fail("deductFee() Method not yet implemented");
-		
-	}
-	
-	@Test
-	public void testCheckReport() throws Exception
-	{
-		fail("checkReport() Method not yet implemented");
-	}
-	
-	@Test
-	public void testReviewAppl() throws Exception
-	{
-		fail("reviewAppl() Method not yet implemented");
-	}
-	
 
 }
